45e9e2813caf68db69c190af9fb74b232cca9bba
dongso03 <dongso03@naver.com>
2024-03-19 18:12:57 +0900

    가짜목록 생성하여 web상에 페이지로 나타내기

---
+++ beerfaker/src/kr/co/greenart/BeerFaker.java
@@ -0,0 +1,32 @@
+package kr.co.greenart;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.github.javafaker.Faker;
+
+public class BeerFaker {
+	public static void main(String[] args) {
+		Faker faker = new Faker();
+		
+		System.out.println(faker.beer().name());
+		System.out.println(faker.beer().name());
+		System.out.println(faker.beer().name());
+		System.out.println(faker.beer().name());
+		String sql = "Insert into beer (name, price) values (?,?)";
+		try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_db", "root","root");
+				PreparedStatement stmt = conn.prepareStatement(sql)){
+			for(int i = 0; i<100;i++) {
+				stmt.setString(1, faker.beer().name());
+				stmt.setInt(2, faker.number().numberBetween(4000, 6000));
+				stmt.addBatch();
+			}
+			stmt.executeBatch();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+				
+	}
+}
---
+++ beerfaker/src/kr/co/greenart/DragonballFaker.java
@@ -0,0 +1,20 @@
+package kr.co.greenart;
+
+import com.github.javafaker.Faker;
+
+public class DragonballFaker {
+	public static void main(String[] args) {
+		Faker faker = new Faker();
+		System.out.println(faker.dragonBall().character());
+		System.out.println(faker.dragonBall().character());
+		System.out.println(faker.dragonBall().character());
+		System.out.println(faker.dragonBall().character());
+		System.out.println(faker.dragonBall().character());
+		System.out.println(faker.dragonBall().character());System.out.println(faker.dragonBall().character());
+		System.out.println(faker.dragonBall().character());
+		System.out.println(faker.dragonBall().character());
+		System.out.println(faker.dragonBall().character());
+		
+		
+	}
+}
